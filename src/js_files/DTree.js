daa179277f56ea62d6586142735acd0067eac5eb
avnerkhan <avnerkhan@utexas.edu>
2019-05-22 00:17:33 -0500

    Added Page Enum

--- src/js_files/DTree.js
+++ src/js_files/DTree.js
@@ -1,2 +1,15 @@
 import React from "react"
 import Tree from "react-d3-tree"
+
+
+class DTree extends React.Component {
+    render() {
+        return(
+            <div>
+                This is DTreee
+            </div>
+        )
+    }
+}
+
+export default DTree
--- src/js_files/Pick.js
+++ src/js_files/Pick.js
@@ -2,10 +2,25 @@ import React from 'react';
 import '../css_files/Pick.css';
 import Button from 'react-bootstrap/Button'
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import DTree from './DTree'
 
 
 
 class Pick extends React.Component {
+
+
+  constructor(props) {
+    super(props)
+    this.PageEnums = {
+      DTREE: 0
+    }
+
+  }
+
+  goToAlgorithimPage(id) {
+    console.log(id)
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +29,7 @@ class Pick extends React.Component {
             Pick an algorithim
           </p>
           <ButtonToolbar>
-            <Button variant="primary">Decision Tree</Button>
+            <Button onClick={(e) => this.goToAlgorithimPage(this.PageEnums.DTREE, e)} variant="primary">Decision Tree</Button>
           </ButtonToolbar>
         </header>
       </div>
