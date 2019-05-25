3f8332afd5ff51afdbd1aa6e1bfd259aa71b53ac
avnerkhan <avnerkhan@utexas.edu>
2019-05-25 16:43:45 -0500

    KNN generate random points

--- src/js_files/KNN.js
+++ src/js_files/KNN.js
@@ -1,59 +1,62 @@
 import React from 'react'
-import {ScatterChart, Scatter, CartesianGrid, XAxis, YAxis, ZAxis, Tooltip, Legend} from 'recharts'
+import {ScatterChart, Scatter, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts'
+import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 import '../css_files/App.css'
 
 class KNN extends React.Component {
     constructor(props) {
         super(props)
 
-        this.classData = [
-            {
-                "x": 200,
-                "y": 260,
-                "z": 240
-              },
-              {
-                "x": 240,
-                "y": 290,
-                "z": 220
-              },
-              {
-                "x": 190,
-                "y": 290,
-                "z": 250
-              },
-              {
-                "x": 198,
-                "y": 250,
-                "z": 210
-              },
-              {
-                "x": 180,
-                "y": 280,
-                "z": 260
-              },
-              {
-                "x": 210,
-                "y": 220,
-                "z": 230
-              }
-        ]
+        this.state = {
+          positiveData: [],
+          negativeData: []
+        }
 
+    }
+
+    generateRandomData(length=100, max=100) {
+
+      let positiveDataNew = []
+      let negativeDataNew = []
+
+        for(let i =0; i < length; i++) {
+          let randomX = Math.floor(Math.random() * max)
+          let randomY = Math.floor(Math.random() * max)
+          let randomZ = Math.floor(Math.random() * max)
+          let entry = {x: randomX, y:randomY, z: randomZ}
+           if(i % 2 === 0) {
+              positiveDataNew.push(entry)
+           } else {
+              negativeDataNew.push(entry)
+           }
+        }
+
+        this.setState({
+          positiveData: positiveDataNew,
+          negativeData: negativeDataNew,
+          unlabeledData:[]
+        })
     }
 
     render() {
         return (
             <div className="App">
                 <div className="App-header">
-                     <ScatterChart width={800} height={400} >
+                     <ScatterChart width={1000} height={400} margin={{top: 20, right: 20, bottom: 20, left: 20}} >
                         <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="x" name="stature" unit="cm" />
-                        <YAxis dataKey="y" name="weight" unit="kg" />
-                        <ZAxis dataKey="z" range={[64, 144]} name="score" unit="km" />
+                        <XAxis dataKey="x" name="x" domain={[0,100]}  />
+                        <YAxis dataKey="y" name="y" />
                         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                         <Legend />
-                        <Scatter name="Class One" data={this.classData} />
+                        <Scatter name="Positive" data={this.state.positiveData} fill="#32CD32" />
+                        <Scatter name="Negative" data={this.state.negativeData} fill="#FF0000" />
                     </ScatterChart>
+                    <ButtonToolbar>
+                      <Button onClick={() => this.generateRandomData()}>Generate Random Data</Button>
+                      <Button>Add Point</Button>
+                      <Button>Run Algorithim</Button>
+                    </ButtonToolbar>
                 </div>
             </div>
         )
