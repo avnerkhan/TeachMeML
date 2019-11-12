29063bdfab8e2b57787c867935175042097d0772
avnerkhan <avnerkhan@utexas.edu>
2019-11-12 17:54:08 -0600

    Added some more info

--- src/components/learn/AprioriLearn.js
+++ src/components/learn/AprioriLearn.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Image } from "react-bootstrap";
+import Timeseries from "../../Images/Timeseries.png";
 
 class AprioriLearn extends React.Component {
   render() {
@@ -6,7 +8,19 @@ class AprioriLearn extends React.Component {
       <div>
         <h1>Apriori Learn</h1>
         <h2>What is timeseries data</h2>
-        <p>Info</p>
+        <p>
+          Time series data is very different from the previous pieces of data
+          that we have seen before. Instead of having features and labels, time
+          series data looks like this.
+        </p>
+        <Image src={Timeseries} />
+        <p>
+          You can imagine this entire table to represent the transactions of a
+          singular customer. The TID column simply stands for transaction ID.
+          Each row represents that customer's shopping cart on that particular
+          day. For each row, you can’t have duplicate item. For example, row
+          with TID 1 can’t have two instances of bread.
+        </p>
         <h2>When to use FPTree vs Apriori</h2>
         <p>Info</p>
         <h2>How Apriori Works</h2>
--- src/components/learn/ClusteringLearn.js
+++ src/components/learn/ClusteringLearn.js
@@ -6,7 +6,12 @@ class ClusteringLearn extends React.Component {
       <div>
         <h1>Clustering Learn</h1>
         <h2>What is clustering?</h2>
-        <p>Info</p>
+        <p>
+          Clustering is a generalization of unsupervised algorithms. This means
+          that our data comes without any labels, and it is our job to create
+          labels. There are two algorithms that we will focus on for clustering,
+          KMeans and DBScan.
+        </p>
         <h2>When to use KMeans and DBSCAN</h2>
         <p>Info</p>
         <h2>How KMeans Works</h2>
