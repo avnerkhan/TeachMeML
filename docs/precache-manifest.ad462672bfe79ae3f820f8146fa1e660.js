a71dd08f759b80cfdcfbb97efb7c759a68c48b9e
avnerkhan <avnerkhan@utexas.edu>
2020-01-09 18:08:58 -0600

    Added docs for static hosting

---
+++ docs/precache-manifest.ad462672bfe79ae3f820f8146fa1e660.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca275c180e325923bea57ddd165e9f69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6c370dbc572a4f0dc0c",
+    "url": "/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "0787440af932a2c1dc1b",
+    "url": "/static/css/main.8ca5435f.chunk.css"
+  },
+  {
+    "revision": "b6c370dbc572a4f0dc0c",
+    "url": "/static/js/2.ea271ebe.chunk.js"
+  },
+  {
+    "revision": "0787440af932a2c1dc1b",
+    "url": "/static/js/main.6fe7e408.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "32488250a2a42f0881ae40ce3e1533eb",
+    "url": "/static/media/1-set.32488250.png"
+  },
+  {
+    "revision": "ff6088bb0cb49b69c8c3fb23a4947aa5",
+    "url": "/static/media/2-set.ff6088bb.png"
+  },
+  {
+    "revision": "95d85957df632936efb392e0d8751659",
+    "url": "/static/media/3-set.95d85957.png"
+  },
+  {
+    "revision": "5dce3375de3798f776f94e53e012a119",
+    "url": "/static/media/BulletPoints.5dce3375.png"
+  },
+  {
+    "revision": "80ad8bab0a8486fe6e55e46bc0ec4297",
+    "url": "/static/media/Conditional.80ad8bab.png"
+  },
+  {
+    "revision": "fe9683bddfb269a2b2e55d09bd9e5018",
+    "url": "/static/media/Conf.fe9683bd.png"
+  },
+  {
+    "revision": "334944b16a4e9e00c113826c8e1e637d",
+    "url": "/static/media/Data.334944b1.png"
+  },
+  {
+    "revision": "bfd9b58146263d910f757712ccf9f9c3",
+    "url": "/static/media/Entropy.bfd9b581.png"
+  },
+  {
+    "revision": "5dc839871acf36073cf397b870e56632",
+    "url": "/static/media/Euclid.5dc83987.png"
+  },
+  {
+    "revision": "043000eeb245e379c72f02390e17cc81",
+    "url": "/static/media/FPTree.043000ee.png"
+  },
+  {
+    "revision": "00a6e3b922035902ca21eee08537375a",
+    "url": "/static/media/GainRatio.00a6e3b9.png"
+  },
+  {
+    "revision": "b740d995a1e24a9311bb010373031bf5",
+    "url": "/static/media/Gini.b740d995.png"
+  },
+  {
+    "revision": "e711326e67c36f329617db36f7dad430",
+    "url": "/static/media/Graphs.e711326e.png"
+  },
+  {
+    "revision": "c0d3649951fb56aab7202c77de7c2615",
+    "url": "/static/media/Impurities.c0d36499.png"
+  },
+  {
+    "revision": "aeea75293fc1bb0f36a005e2eea3c74b",
+    "url": "/static/media/Labeled.aeea7529.png"
+  },
+  {
+    "revision": "ba6476ab0e659a0b5ff8c5c2beb1d455",
+    "url": "/static/media/LabeledFirst.ba6476ab.png"
+  },
+  {
+    "revision": "1c94b965ebaa2956bb5e7d9479be3cc2",
+    "url": "/static/media/LabeledSecond.1c94b965.png"
+  },
+  {
+    "revision": "c109f6dc647949c8ed9dbfb8b15ba06a",
+    "url": "/static/media/Labeled_cluster.c109f6dc.png"
+  },
+  {
+    "revision": "696801fa528e900afc31fa6970705a8c",
+    "url": "/static/media/SomeTree.696801fa.png"
+  },
+  {
+    "revision": "561f4984da0c1962b283c9060f9e2468",
+    "url": "/static/media/Sorted.561f4984.png"
+  },
+  {
+    "revision": "25467f0eb7bf7b4bf59b0c3d7dbe6c68",
+    "url": "/static/media/Split.25467f0e.png"
+  },
+  {
+    "revision": "b3a2e3ea9970f7b66f57463e2d2a9d3d",
+    "url": "/static/media/SplitInfo.b3a2e3ea.png"
+  },
+  {
+    "revision": "413f30535c548e4d3d4b309844ce91c3",
+    "url": "/static/media/Timeseries.413f3053.png"
+  },
+  {
+    "revision": "e9b535edcb23ec2103d0e4951f726586",
+    "url": "/static/media/Tree.e9b535ed.png"
+  },
+  {
+    "revision": "e91d2ad2e834e47b1caf6e2b1136000f",
+    "url": "/static/media/Unlabeled.e91d2ad2.png"
+  },
+  {
+    "revision": "18e74735194ba372592c03a4b868c84d",
+    "url": "/static/media/UnlabeledCluster.18e74735.png"
+  },
+  {
+    "revision": "541fb320bce02cddcda3067b9fe3140e",
+    "url": "/static/media/Unlabeled_cluster.541fb320.png"
+  },
+  {
+    "revision": "7012a73bd0785eb3151a557db56d0923",
+    "url": "/static/media/back.7012a73b.png"
+  },
+  {
+    "revision": "ea9eb7430a92dfeda3530f1774eddf1c",
+    "url": "/static/media/check.ea9eb743.png"
+  },
+  {
+    "revision": "bdab74a26adb3b404c113d8731ad2669",
+    "url": "/static/media/cool.bdab74a2.jpg"
+  },
+  {
+    "revision": "17147fc8bfc46704ec2f6f86b6ca772e",
+    "url": "/static/media/icon.17147fc8.png"
+  },
+  {
+    "revision": "317d22ea3378c3715bce4f8c735be7da",
+    "url": "/static/media/reset.317d22ea.png"
+  },
+  {
+    "revision": "84866c1a3994348751dddad302b02d6a",
+    "url": "/static/media/shuffle.84866c1a.png"
+  }
+]);
